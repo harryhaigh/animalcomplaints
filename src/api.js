9aec9864b1ebebd0237bebfe89b9d6b1d9b532b8
Harold Haigh <Harold.Haigh@qed.qld.gov.au>
2022-10-19 20:34:11 +1000

    Axios get 10k records added

---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// Get 10,000 records
+export default {
+	getData: () => {
+		return axios({
+			method: "get",
+			url: `https://www.data.brisbane.qld.gov.au/data/api/3/action/datastore_search?resource_id=87114bf7-3da5-497e-a481-337a5cc197eb&limit=10000`,
+			headers: {
+				accept: "application/json",
+			},
+		})
+			.then((response) => {
+				return response;
+			})
+			.catch((error) => {
+				return `Error: ${error}`;
+			});
+	},
+};
---
+++ src/functions/getData.js
@@ -0,0 +1,5 @@
+import api from "../api";
+
+export const getData = () => {
+	return console.log(api.getData());
+};
